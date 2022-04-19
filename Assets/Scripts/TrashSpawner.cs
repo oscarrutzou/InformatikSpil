a561ffea0e76db3892ad6005d9bd1d17eb68d3db
oscarrutzou <91065000+oscarrutzou@users.noreply.github.com>
2022-04-19 14:57:00 +0200

    sd

--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class TrashSpawner : MonoBehaviour
 {
+    //De forskellige Prefabs som vi bruger
+    //Lav probabilities i dem
+    public GameObject Hest;
+    public float respawnTime = 1.0f;
+    private Vector2 pos;
+
+    public GameObject spawner;
+
+    void Awake()
+    {
+        
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    private void spawnTrash()
+    {
+
+    }
+
 
     // Update is called once per frame
     void Update()
