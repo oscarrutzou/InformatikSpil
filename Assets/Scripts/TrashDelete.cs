f8cf5c983cc92da97dede15a1b9e62494c934a84
ShiRongAlways <90238871+ShiRongAlways@users.noreply.github.com>
2022-04-19 14:56:28 +0200

    Skrald bevæger sig

--- Assets/Scripts/TrashDelete.cs
+++ Assets/Scripts/TrashDelete.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class TrashDelete : MonoBehaviour
 {
+
+    public float moveSpeed = 5;
     //Til hvor meget score de hver objekter har.
     public int score;
     //Laver en variable så man kan kontakte "Display" scriptet.
     private Display display;
-
+    public Rigidbody2D rb2D;
+    public float thrust = 1f;
 
     //Den er i Awake, da den skal ske før "void Start."
     void Awake()
     {
         //Tilslutter sig "Display" scriptet.
         display = GameObject.FindObjectOfType<Display>();
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
-
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+       // m_Rigidbody = GetComponent<Rigidbody>();
+    }
     //Når den rammer noget, bliver metoden kaldt.
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -36,7 +44,12 @@ public class TrashDelete : MonoBehaviour
         if (collision.collider.CompareTag("Lava"))
         {
             Destroy(this.gameObject);
+        }
 
+        if (collision.collider.CompareTag("ConveyorBelt"))
+        {
+            Debug.Log("test");
+            rb2D.velocity = new Vector2(0, 10);
         }
 
     }
